307a86cbe006850c1d8c7411bb973e408532e371
Bikram Dhindsa <dhindsa.bikram@gmail.com>
2026-04-24 00:07:07 -0700

    fixing running env

---
+++ src/06 Save a Dictionary/my_sol_dictionary.py
@@ -0,0 +1,11 @@
+import os
+import json
+def save_dict(data,filepath):
+  with open(filepath,'w') as f:
+    json.dump(data,f,indent=4)
+def load_dict(filepath):
+  with open(filepath,'r') as f:
+    return json.load(f)
+if __name__ == '__main__':
+    save_dict({1: 'a', 2: 'b', 3: 'c'}, 'workspaces/level_up_python/testing_my_code.json')
+    print(load_dict('workspaces/level_up_python/testing_my_code.json'))
